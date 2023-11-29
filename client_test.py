eef53a403dc30da93d4c2bd8fb9a9b66825a25a9
whimsicalwill <knipewilliam20@gmail.com>
2023-11-29 00:34:27 -0500

    Initial commit

---
+++ client_test.py
@@ -0,0 +1,14 @@
+from reddit_pb2 import User, Post
+
+user = User(user_id="user123")
+post = Post(title="Example Title", text="This is a post", score=10)
+print(user)
+print(post)
+
+# Serialize
+serialized_post = post.SerializeToString()
+
+# Deserialize
+new_post = Post()
+new_post.ParseFromString(serialized_post)
+print(new_post)
